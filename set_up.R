069ea78a57e1f15aea6d6a89462b85d78d641b19
araupontones <43657130+araupontones@users.noreply.github.com>
2021-03-12 16:15:45 +0100

    reomove path definition from set_up

--- set_up.R
+++ set_up.R
@@ -14,20 +14,11 @@ p_load(lubridate, tidyverse, httr, rio, glue, RCurl,sf, leaflet, leaflet.extras)
 #' 
 #' Survey Solutions
 
-ssuser = "araupontones"
-sspassword = "Seguridad1"
-sserver = "http://www.pulpodata.solutions"
 
 
 #PATHS ------------------------------------------------------------------------
 
-if(Sys.getenv("USERNAME")=="andre"){
-  
-  dropbox = "C:/Users/andre/Dropbox/SS"
-}
 
-dir_ss_downloads = file.path(dropbox, "downloads")
-dir_raw = file.path(dropbox, "raw")
 
 
 temporDir = tempdir()
